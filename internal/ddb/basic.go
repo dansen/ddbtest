bd8316b93c32d517f0c33d42dd531c2caed53ff2
dansen <370283593@qq.com>
2024-04-23 10:14:25 +0800

    ddb test

--- internal/ddb/basic.go
+++ internal/ddb/basic.go
@@ -1,53 +1,71 @@
 package ddb
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 type DDB struct {
-	DB *dynamodb.DynamoDB
+	DB *dynamodb.Client
 }
 
 var DB *DDB = &DDB{}
 
 func (c *DDB) Connect() {
-	sess, err := session.NewSession(&aws.Config{
-		Region:   aws.String("us-west-1"),
-		Endpoint: aws.String("http://localhost:8000"),
-	})
+	cre := config.WithCredentialsProvider(
+		aws.CredentialsProviderFunc(
+			func(_ context.Context) (aws.Credentials, error) {
+				return aws.Credentials{
+					AccessKeyID:     ddb.Credentials.AccessKeyID,
+					SecretAccessKey: ddb.Credentials.SecretAccessKey,
+					SessionToken:    ddb.Credentials.SessionToken,
+					Source:          ddb.Credentials.Source,
+				}, nil
+			},
+		),
+	)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+			return aws.Endpoint{URL: "http://localhost:8000"}, nil
+		})))
 
 	if err != nil {
 		panic(err)
 	}
 
-	c.DB = dynamodb.New(sess)
+	client := dynamodb.NewFromConfig(cfg)
+
+	c.DB = client
 	c.CreateTable()
 }
 
 func (c *DDB) CreateTable() {
 	input := &dynamodb.CreateTableInput{
-		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+		AttributeDefinitions: []types.AttributeDefinition{
 			{
 				AttributeName: aws.String("Id"),
-				AttributeType: aws.String("N"),
+				AttributeType: types.ScalarAttributeTypeN,
 			},
 		},
-		KeySchema: []*dynamodb.KeySchemaElement{
+		KeySchema: []types.KeySchemaElement{
 			{
 				AttributeName: aws.String("Id"),
-				KeyType:       aws.String("HASH"),
+				KeyType:       types.KeyTypeHash,
 			},
 		},
-		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+		ProvisionedThroughput: &types.ProvisionedThroughput{
 			ReadCapacityUnits:  aws.Int64(5),
 			WriteCapacityUnits: aws.Int64(5),
 		},
 		TableName: aws.String("MyTable"),
 	}
 
-	_, err := c.DB.CreateTable(input)
+	_, err := c.DB.CreateTable(context.TODO(), input)
 	if err != nil {
 		panic(err)
 	}
